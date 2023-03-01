c9464e380dfd827ec5641f992da804f36cd11467
juxinyu <1902732893@qq.com>
2023-03-01 09:42:14 +0800

    fix🐛:  修改  js代码加分号

--- js/language.js
+++ js/language.js
@@ -27,29 +27,25 @@ var arrLang = {
 
 };
 
-// The default language is en
 
 var lang = "en";
-
-// Check for localStorage support
-
 if ('localStorage' in window) {
 
 	var lang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
 	if (lang === 'id') {
-		document.getElementById("ChangeLanguage").options[0].selected = true
+		document.getElementById("ChangeLanguage").options[0].selected = true;
 	} else {
-		document.getElementById("ChangeLanguage").options[1].selected = true
-	}
+		document.getElementById("ChangeLanguage").options[1].selected = true;
+	};
 	if (!Object.keys(arrLang).includes(lang)) lang = 'en';
 
-}
+};
 
 document.ready(function() {
 	let list = document.getElementsByClassName('lang');
 	for (let i = 0; i < list.length; i++) {
 		list[i].innerHTML = arrLang[lang][list[i].getAttribute('key')]
-	}
+	};
 });
 
 // get/set the selected language
@@ -58,7 +54,6 @@ function change() {
 	let obj = document.getElementById('ChangeLanguage');
 	let index = document.getElementById('ChangeLanguage').selectedIndex;
 	let lang = obj[index].value;
-	// update localStorage key
 	if ('localStorage' in window) {
 		localStorage.setItem('lang', lang)
 	};
@@ -67,4 +62,4 @@ function change() {
 	for (let i = 0; i < list.length; i++) {
 		list[i].innerHTML = arrLang[lang][list[i].getAttribute('key')]
 	};
-}
+};
